9e1bc32c80489731cabc75a0433933971a25c8ba
tueritox <62852919+tueritox@users.noreply.github.com>
2023-08-24 16:51:55 +0200

    Update alpha_vantage_key.py

--- alpha_vantage_key.py
+++ alpha_vantage_key.py
@@ -1,4 +1,5 @@
-
+"""Please go to www.alphvantage.com and get your free KEY, and insert it as the k variable in the code below"""
 def key():
-    k = 'YO3HN54BJ1HUI3E6'
+    #in between commas insert your KEY here
+    k = 'INSERT YOUR ALPHA VANTAGE KEY HERE'
     return k
